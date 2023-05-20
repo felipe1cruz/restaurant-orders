0c134b27048e31d7f9c90cd00f86beb749df0f2c
felipe-cruz <piradous@gmail.com>
2023-05-20 11:04:32 -0300

    tests: Req 01 - test_ingredient.py

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,16 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("bacon")
+    assert ingredient.name == "bacon"
+    # testa atributo restrictions
+    assert ingredient.restrictions == {"ANIMAL_MEAT", "ANIMAL_DERIVED"}
+    # testa método mágico __repr__
+    assert repr(ingredient) == "Ingredient('bacon')"
+    # testa método mágico __eq__
+    ingredient2 = Ingredient("bacon")
+    ingredient3 = Ingredient("cheese")
+    assert ingredient == ingredient2
+    assert ingredient != ingredient3
+    # testa método mágico __hash__
+    assert hash(ingredient) == hash(ingredient2)
